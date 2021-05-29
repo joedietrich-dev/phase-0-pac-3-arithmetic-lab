e0b3653f9b04146818fe7cf9673f4f8e7910a482
Joe Dietrich <joe@joedietrich.net>
2021-05-29 15:19:57 -0400

    Assignment complete

---
+++ index.js
@@ -0,0 +1,20 @@
+function add(a,b){
+  return a + b;
+}
+function subtract(a,b){
+  return a - b;
+}
+function multiply(a,b){
+  return a * b;
+}
+function divide(a,b){
+  return a / b;
+}
+function makeInt(num){
+  return parseInt(num, 10);
+}
+function preserveDecimal(num){
+  return parseFloat(num);
+}
+function increment(num){return ++num;}
+function decrement(num){return --num;}
